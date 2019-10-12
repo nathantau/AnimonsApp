8da48c5ce302a9acaecfb05ed52128df279bcf37
Nathan Au <nathan.au1280@gmail.com>
2019-10-12 19:14:27 -0400

    Got camera screen workign

--- CustomComponents/CameraScreen.js
+++ CustomComponents/CameraScreen.js
@@ -1,25 +1,94 @@
-import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  TextInput,
-  Button,
-  TouchableNativeFeedback
-} from 'react-native';
+import React, {Component, PureComponent} from 'react';
+import { RNCamera } from 'react-native-camera';
+'use strict';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-class CameraScreen extends Component {
+class CameraScreen extends PureComponent {
 
-		static navigationOptions = {
-				header: null
+	static navigationOptions = {
+			header: null
+	}
+
+  constructor(props) {
+	super(props);
+
+	this.state = {
+	  path: null,
+	};
+  }
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<RNCamera
+					ref={ref => {
+						this.camera = ref;
+					}}
+					style={styles.preview}
+					type={RNCamera.Constants.Type.back}
+					captureAudio={false}
+
+					flashMode={RNCamera.Constants.FlashMode.on}
+					androidCameraPermissionOptions={{
+						title: 'Permission to use camera',
+						message: 'We need your permission to use your camera',
+						buttonPositive: 'Ok',
+						buttonNegative: 'Cancel',
+					}}
+					androidRecordAudioPermissionOptions={{
+						title: 'Permission to use audio recording',
+						message: 'We need your permission to use your audio',
+						buttonPositive: 'Ok',
+						buttonNegative: 'Cancel',
+					}}
+					onGoogleVisionBarcodesDetected={({ barcodes }) => {
+						console.log(barcodes);
+					}}
+				/>
+				<View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+					<TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+						<Text style={{ fontSize: 14 }}> SNAP </Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+		);
+	}
+
+	takePicture = async() => {
+		if (this.camera) {
+			const options = { quality: 0.5, base64: true };
+			const data = await this.camera.takePictureAsync(options);
+			console.log(data.uri);
 		}
-		render(){
-		return <Text>Hello world</Text>
+	};
 
-		}
+
+
+
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
 
 export default CameraScreen
--- android/app/src/main/java/com/grouplisten/MainActivity.java
+++ android/app/src/main/java/com/grouplisten/MainActivity.java
@@ -1,6 +1,7 @@
 package com.grouplisten;
 
 import com.facebook.react.ReactActivity;
+import org.reactnative.camera.RNCameraPackage;
 
 public class MainActivity extends ReactActivity {
 
